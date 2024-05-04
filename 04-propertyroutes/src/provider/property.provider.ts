07d7305d5425a9fce790a3a204b8868f0258f7e0
Gaurav Soni <gauravsoni12345@gmail.com>
2024-05-04 23:42:04 +0400

    Added authorization folder

--- 04-propertyroutes/src/provider/property.provider.ts
+++ 04-propertyroutes/src/provider/property.provider.ts
@@ -3,7 +3,10 @@ import ListingsAndReviewsModel from '../model/Property';
 export class PropertyProvider {
 
   async createProperty(property) {
-    const propertyData = new ListingsAndReviewsModel(property);
+    const parsedPropertyInput = { ...property, images: { picture_url: property?.image } }
+    delete parsedPropertyInput['image'];
+
+    const propertyData = new ListingsAndReviewsModel(parsedPropertyInput);
 
     try {
       const resp = await propertyData.save();
---
+++ 06-authorization/src/app.ts
@@ -0,0 +1,18 @@
+import * as express from 'express';
+import { UserRoutes } from './routes/user.routes';
+import * as bodyParser from 'body-parser';
+import * as CORS from 'cors';
+
+import './config/mongo_init';
+import { PropertyRoutes } from './routes/property.routes';
+
+const app = express();
+app.use(CORS({ origin: "http://localhost:5173" }));
+
+app.use(bodyParser.json());
+
+app.get("/health", (req, res) => res.status(200).send("OK"));
+app.use('/user', new UserRoutes().routes);
+app.use('/property', new PropertyRoutes().routes);
+
+export default app;
